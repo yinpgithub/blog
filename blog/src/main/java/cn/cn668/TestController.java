c9481f0695b5f93aabc62459d401e1fac191a9fc
尹鹏 <yinp0000@163.com>
2020-04-12 10:15:57 +0800

    基础框架搭建

---
+++ blog/src/main/java/cn/cn668/TestController.java
@@ -0,0 +1,14 @@
+package cn.cn668;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping(value = "test")
+	public String test (){
+		return "Hello Spring Boot";
+	}
+
+}
